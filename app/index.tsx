86c3635b44b67ef10a68bef7ca4f4af17aa305a9
Michał Marchewczyk <dev@marchewczyk.eu>
2023-08-12 22:12:12 +0200

    Setup initial redirect to login screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,14 @@
-import { Link } from 'expo-router';
+import { Redirect, useRootNavigationState } from 'expo-router';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-import TextButton from '../components/TextButton';
-import { COLORS } from '../styles/colors';
 
 function Index() {
-  return (
-    <View style={styles.container}>
-      <Text style={{ fontFamily: 'Poppins-Bold', fontSize: 70, color: COLORS.plum['500'] }}>Chatty.</Text>
-      <Link href="/login" asChild>
-        <TextButton title="Log in" />
-      </Link>
-    </View>
-  );
-}
+  const rootNavigationState = useRootNavigationState();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: COLORS.blue['300'],
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 24,
-    gap: 24,
-  },
-});
+  if (!rootNavigationState.key) {
+    return null;
+  }
+
+  return <Redirect href="/login" />;
+}
 
 export default Index;
