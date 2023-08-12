4cb2adb5e944dd8cd5debd1cc7469f94b06a6a74
Michał Marchewczyk <dev@marchewczyk.eu>
2023-08-12 22:13:33 +0200

    Delete unused fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,13 +7,9 @@ import Provider from '../components/Provider';
 function Layout() {
   const [fontsLoaded] = useFonts({
     'Poppins-Bold': require('../assets/fonts/Poppins/Poppins-Bold.ttf'),
-    'Poppins-BoldItalic': require('../assets/fonts/Poppins/Poppins-BoldItalic.ttf'),
-    'Poppins-Italic': require('../assets/fonts/Poppins/Poppins-Italic.ttf'),
     'Poppins-Medium': require('../assets/fonts/Poppins/Poppins-Medium.ttf'),
-    'Poppins-MediumItalic': require('../assets/fonts/Poppins/Poppins-MediumItalic.ttf'),
     'Poppins-Regular': require('../assets/fonts/Poppins/Poppins-Regular.ttf'),
     'Poppins-SemiBold': require('../assets/fonts/Poppins/Poppins-SemiBold.ttf'),
-    'Poppins-SemiBoldItalic': require('../assets/fonts/Poppins/Poppins-SemiBoldItalic.ttf'),
   });
 
   if (!fontsLoaded) {
