d6f3611f6749784d742c022299d9cfcf47503887
Michał Marchewczyk <dev@marchewczyk.eu>
2023-08-13 15:15:57 +0200

    Add splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,24 @@
 import { useFonts } from 'expo-font';
-import { Slot } from 'expo-router';
-import React from 'react';
+import { Slot, SplashScreen } from 'expo-router';
+import React, { useEffect } from 'react';
 
 import Provider from '../components/core/Provider';
 
+SplashScreen.preventAutoHideAsync();
+
 function Layout() {
-  const [fontsLoaded] = useFonts({
+  const [fontsLoaded, fontError] = useFonts({
     'Poppins-Bold': require('../assets/fonts/Poppins/Poppins-Bold.ttf'),
     'Poppins-Medium': require('../assets/fonts/Poppins/Poppins-Medium.ttf'),
     'Poppins-Regular': require('../assets/fonts/Poppins/Poppins-Regular.ttf'),
     'Poppins-SemiBold': require('../assets/fonts/Poppins/Poppins-SemiBold.ttf'),
   });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
 
   if (!fontsLoaded) {
     return null;
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,14 @@
 import { Redirect, useRootNavigationState } from 'expo-router';
 import React from 'react';
+import { View } from 'react-native';
+
+import { COLORS } from '../styles/colors';
 
 function Index() {
   const rootNavigationState = useRootNavigationState();
 
   if (!rootNavigationState.key) {
-    return null;
+    return <View style={{ backgroundColor: COLORS.blue['300'], flex: 1 }} />;
   }
 
   return <Redirect href="/login" />;
